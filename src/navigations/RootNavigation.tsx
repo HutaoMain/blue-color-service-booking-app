73109fb1ae08bbf9762556e59008e295f5db0958
HutaoMain <alimohamedalcantara@gmail.com>
2024-08-03 12:21:21 +0800

    add bottom tab navigation and initial chatscreen and profile screen

--- src/navigations/RootNavigation.tsx
+++ src/navigations/RootNavigation.tsx
@@ -1,16 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import HomeStackNavigation from "./HomeStackNavigation";
-// import AuthStackNavigation from "./AuthStackNavigation";
-// import useAuthStore from "../zustand/AuthStore";
-// import BottomTabNavigation from "./BottomTabNavigation";
+import BottomTabNavigation from "./BottomTabNavigation";
 
 const RootNavigation = () => {
   // const user = useAuthStore((state) => state.user);
   return (
     <NavigationContainer>
       {/* {user ? <BottomTabNavigation /> : <AuthStackNavigation />} */}
-      <HomeStackNavigation />
+      <BottomTabNavigation />
     </NavigationContainer>
   );
 };
